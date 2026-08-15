94dd05f9a2c01d4b2ff5f5aa677d74c06f798d70
sxprr <magalajawad@gmail.com>
2026-08-15 21:25:20 +0100

    Added Gamemanager that will utilize Unity's built in Eventbus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
-   
 {
+    public UnityEvent OnInteraction;
+    public UnityEvent OnEnemySight;
+    public UnityEvent OnVictoryAchieved;
+
+    private void Awake()
+    {
+        LogHandler.Log($"<color=green>SUCCESS:</color> {gameObject.name} has entered the chat.");
+    }
+
+    public static void TriggerMoonResist()
+    {
+        
+    }
+
     // The event bus belongs here.
 }
